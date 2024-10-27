5109017f9d8ff85c58aec4a1000298f1a17fcbf4
alok2011 <alokranjantiwaricse@gmail.com>
2024-10-27 14:39:42 +0530

    update

--- src/main/java/Chetu/client/Runner.java
+++ src/main/java/Chetu/client/Runner.java
@@ -19,7 +19,7 @@ public class Runner {
 
 //===========================Java Based Configuration=========================================
 
-		Employee Alok = new Employee(1,"Hariom","Male","Noida");
+		Employee Alok = new Employee(1,"Alok","Male","Noida");
 		
 		SessionFactory sf = Hibernate_Configuration.getSessionFactory();
 		Session session = sf.openSession();
